bb8ce60e7701360a73b948eb8f1cf2dae543dda3
Masambu Emmanuel <jemasambu@gmail.com>
2022-03-23 18:36:34 +0300

    Edit Contact.js file

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -3,7 +3,7 @@ import React from "react";
 const ContactList = () => {
     return (
         <div className="ui celled list">
-            Contact List
+            Contact List will Go here!
         </div>
     )
 }
